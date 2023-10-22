2432b5a5589c8319f2a2b553adae3a529fae432b
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-22 22:52:25 +0200

    ,

--- js/script.js
+++ js/script.js
@@ -3,35 +3,86 @@
 // 2. Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // 3. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
-// Evento Bottone
-btn.addEventListener('click',function(){
-    // numero di quadrati
-    const squareNumber = 64;
-    // Griglia
+// // Evento Bottone
+// btn.addEventListener('click',function(){
+//     // numero di quadrati
+//     const squareNumber = 64;
+//     // Griglia
+//     const playground = document.getElementById('playground');
+//     playground.innerHTML = ''
+//     // Stampa dei Quadrati
+//     for(let i = 0; i < squareNumber; i++){
+//         let square = drawSquare(i,squareNumber);
+//         console.log(square);
+//         // Aggiungi il quadrato
+//         playground.append(square);
+//     }
+// })
+
+// // FUNCTION STAMPA DEI QUADRATI
+// function drawSquare(squareI,squareNumber){
+//     const squareWidth = Math.sqrt(squareNumber);
+//     const square = document.createElement('div');
+//     square.classList.add('square');
+//     square.style.width = `calc(100% / ${squareWidth})`;
+//     square.style.height = square.style.width;
+//     square.innerHTML = squareI + 1;
+//     // ACTIVE SUL CLICK AL QUADRATOO
+//     square.addEventListener('click',function(){
+//         square.classList.add('active');
+//     });
+//     return square;
+// };
+"use strict";
+
+campoMinato();
+function campoMinato(){
+    const btn = document.querySelector('button');
+
+btn.addEventListener('click', function(){
+    const level = document.getElementById('level').value;
+    let numSquare = selectLevel(level);
+    console.log(numSquare);
+    const squareWidth = Math.sqrt(numSquare);
     const playground = document.getElementById('playground');
-    playground.innerHTML = ''
-    // Stampa dei Quadrati
-    for(let i = 0; i < squareNumber; i++){
-        let square = drawSquare(i,squareNumber);
-        console.log(square);
-        // Aggiungi il quadrato
+    playground.innerHTML = ' ';
+    for(let i = 0; i < numSquare; i++){
+        let square = drawSquare(i+1,squareWidth);
         playground.append(square);
     }
-})
+});
 
-// FUNCTION STAMPA DEI QUADRATI
-function drawSquare(squareI,squareNumber){
-    const squareWidth = Math.sqrt(squareNumber);
+function drawSquare(content,squareWidth){
     const square = document.createElement('div');
     square.classList.add('square');
     square.style.width = `calc(100% / ${squareWidth})`;
     square.style.height = square.style.width;
-    square.innerHTML = squareI + 1;
-    // ACTIVE SUL CLICK AL QUADRATOO
-    square.addEventListener('click',function(){
-        square.classList.add('active');
-    });
-    return square;
+    square.innerHTML = content;
+    square.addEventListener('click', drawClick);
+        return square;
+};
+
+function drawClick(){
+    if(!this) return;
+    this.classList.add('active');
+    console.log(this.textContent);
+}
+
+function selectLevel(level){
+    let numSquare; //(level === 'medium') ? 81 : (level === 'difficult') ? 49 : 100;
+    switch(level){
+        case 'medium':
+            numSquare = 81;
+            break;
+        case 'difficult':
+            numSquare = 49;
+            break;
+        default:
+            numSquare = 100;
+    }
+    // console.log(numSquare);
+    return numSquare;
+};
 };
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Consegna
+1. Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+
+2. Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+3. In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+4. La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+
+5. Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.#
